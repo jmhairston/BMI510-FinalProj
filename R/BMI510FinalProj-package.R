319e9a2eacbcefc274cd43ec2d4a8765b85415c8
JaMor Hairston <110644908+jmhairston@users.noreply.github.com>
2024-05-02 18:51:38 -0400

    Add files via upload
    Reuploading all files

---
+++ R/BMI510FinalProj-package.R
@@ -0,0 +1,19 @@
+#' BMI510FinalProj
+#'
+#' `BMI510-FinalProj` is an R package that completes the requirements for the BMI 510 course's final project, on behalf JaMor Hairston.
+#'
+#'\itemize{
+#'   \item \code{\link{logLikBernoulli}}: Calculates the parameter that maximizes the log-likelihood for Bernoulli-distributed data.
+#'   \item \code{\link{survCurv}}: Generates and plots survival curves.
+#'   \item \code{\link{unscale}}: Reverses centering and scaling transformations on data.
+#'   \item \code{\link{pcApprox}}: Reconstructs a dataset using principal components.
+#'   \item \code{\link{standardizeNames}}: Standardizes variable names in a dataset to smallCamelCase.
+#'   \item \code{\link{minimumN}}: Calculates the minimum sample size required for a t-test with specified power and alpha.
+#'   \item \code{\link{downloadRedcapReport}}: Downloads a report from REDCap using an API.
+#'}
+#'
+#' @docType package
+#' @author JaMor Hairston \email{jamor.hairston@emory.edu}
+#'
+#' @name BMI510FinalProj
+"_PACKAGE"
